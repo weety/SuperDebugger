ec6caa9fad7c92575734b1a1ae51f151a46704e8
weety <luohui2320@gmail.com>
2018-11-22 17:31:06 +0800

    Add serial library and improve ui.

---
+++ SerialLib.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 14:57:54 2018
+
+@author: weety
+"""
+
+import sys
+import time
+import serial
+import logging
+import binascii
+import platform
+import threading
+
+if platform.system() == "Windows":
+    from  serial.tools import list_ports
+else:
+    import glob, os, re
+
+class SerialLib(object):
+    def __init__(self, Port="COM1", BaudRate="9600", ByteSize="8", Parity="N", Stopbits="1"):
+        self.port = Port
+        self.baudrate = BaudRate
+        self.bytesize = ByteSize
+        self.parity = Parity
+        self.stopbits = Stopbits
+        self.receive_data = ""
+
+        self.serial = None
+        self.is_opened = False
+        self.is_exist = False
+
+    def open(self, timeout=2):
+        self.serial = serial.Serial()
+        self.serial.port = self.port
+        self.serial.baudrate = self.baudrate
+        self.serial.bytesize = int(self.bytesize)
+        self.serial.parity = self.parity
+        self.serial.stopbits = int(self.stopbits)
+        self.serial.timeout = timeout
+
+        try:
+            self.serial.open()
+            if self.serial.isOpen():
+                self.is_opened = True
+        except Exception as e:
+            self.is_opened = False
+            logging.error(e)
+
+    def close(self):
+        if self.serial:
+            self.serial.close()
+
+    def write(self, data):
+        if self.is_opened:
+            #if isHex:
+            #    data = binascii.unhexlify(data)
+            #self.serial.write(bytes(data))
+            self.serial.write(data)
+
+    def serial_device_monitor(self, func):
+        tmonitor = threading.Thread(target=self.serial_device_monitor_thread, args=(func, ))
+        tmonitor.setDaemon(True)
+        tmonitor.start()
+
+    def serial_device_monitor_thread(self, func):
+        while True:
+            self.is_exist = False
+            if platform.system() == "Windows":
+                for com in list_ports.comports():
+                    if com[0] == self.port:
+                        self.is_exist = True
+                        break
+            elif platform.system() == "Linux":
+                if self.port in self.find_usb_serial():
+                    self.is_exist = True
+
+            if self.is_exist is False:
+                func(self.is_exist)
+            time.sleep(0.8)
+
+    def data_received_func_register(self, func):
+        tDataReceived = threading.Thread(target=self.data_received_thread, args=(func, ))
+        tDataReceived.setDaemon(True)
+        tDataReceived.start()
+    
+    def data_received_thread(self, func):
+        while True:
+            if self.is_opened:
+                try:
+                    number = self.serial.inWaiting()
+                    if number > 0:
+                        data = self.serial.read(number)
+                        if data:
+                            func(data)
+                except Exception as e:
+                    self.is_opened = False
+                    self.serial = None
+                    break
+
+    def find_usb_serial(self, vendor_id=None, product_id=None):
+        '''
+        Find serial device for linux
+        '''
+        tty_devs = list()
+        for dn in glob.glob('/sys/bus/usb/devices/*') :
+            try:
+                vid = int(open(os.path.join(dn, "idVendor" )).read().strip(), 16)
+                pid = int(open(os.path.join(dn, "idProduct")).read().strip(), 16)
+                if  ((vendor_id is None) or (vid == vendor_id)) and ((product_id is None) or (pid == product_id)) :
+                    dns = glob.glob(os.path.join(dn, os.path.basename(dn) + "*"))
+                    for sdn in dns :
+                        for fn in glob.glob(os.path.join(sdn, "*")) :
+                            if  re.search(r"\/ttyUSB[0-9]+$", fn) :
+                                tty_devs.append(os.path.join("/dev", os.path.basename(fn)))
+            except Exception as ex:
+                pass
+        return tty_devs
+
+class testSerialLib(object):
+    def __init__(self):
+        self.serialport = SerialLib(Port="COM5", BaudRate="115200")
+        self.serialport.open()
+        self.serialport.serial_device_monitor(self.myserial_device_monitor)
+        self.serialport.data_received_func_register(self.myserial_data_received)
+
+    def write(self, data):
+        self.serialport.write(data, False)
+
+    def myserial_device_monitor(self, is_exit):
+        if is_exit is False:
+            print("serial device not found")
+            self.serialport.close()
+        else:
+            print("closeed")
+
+    def myserial_data_received(self, data):
+        print(data)
+
+if __name__ == '__main__':
+    serialport = testSerialLib()
+
+    time.sleep(1)
+    serialport.write("Hello, just for test".encode('ascii'))
+    count = 0
+    while count < 9:
+        print("Count: %s"%count)
+        time.sleep(1)
+        count += 1
--- SuperDebugger.py
+++ SuperDebugger.py
@@ -6,26 +6,47 @@ Created on Tue Nov 20 22:18:35 2018
 """
 
 import sys
+import platform
+import logging
 from PyQt5 import QtGui,QtCore
-from PyQt5.QtWidgets import QApplication,QMainWindow,QDialog
+from PyQt5.QtWidgets import QApplication,QMainWindow,QDialog,QColorDialog
 #from ui_Login import Ui_Dialog
 from PyQt5.uic import loadUi
 import pyqtgraph as pg
 import numpy as np
 import array
+import SerialLib as serlib
 
+if platform.system() == "Windows":
+    from serial.tools import list_ports
+else:
+    import glob
+    import os
+    import re
 
 class MainWindow(QMainWindow):  
     def __init__(self, parent = None):  
         super(MainWindow, self).__init__(parent)
         loadUi('SuperDebugger.ui', self)
+        self.waveWidgetInit()
+        self.pushButtonColorSel1.clicked.connect(self.funSelectColor1)
+        self.pushButtonColorSel2.clicked.connect(self.funSelectColor2)
+        self.pushButtonColorSel3.clicked.connect(self.funSelectColor3)
+        self.pushButtonColorSel4.clicked.connect(self.funSelectColor4)
+        self.pushButtonColorSel5.clicked.connect(self.funSelectColor5)
         
+        self.serial_listbox = list()
+        self.detect_all_devices()
+        
+    
+    def waveWidgetInit(self):
         self.pw = pg.PlotWidget(name='Plot1')  ## giving the plots names allows us to link their axes together
         self.waveverticalLayout.addWidget(self.pw)
         
         ## Create an empty plot curve to be filled later, set its pen
         self.p1 = self.pw.plot()
-        self.p1.setPen((200,200,100))
+        self.p1.setPen((255,0,0))
+        #self.p1.setPen(self.pushButtonColorSel1.palette.color())
         
         ## Add in some extra graphics
         #rect = QtGui.QGraphicsRectItem(QtCore.QRectF(0, 0, 1, 1))
@@ -55,7 +76,7 @@ class MainWindow(QMainWindow):
         #updateData()
         
         self.p2 = self.pw.plot()
-        self.p2.setPen((100,200,200))
+        self.p2.setPen((255,255,127))
         
         self.idx2 = 0
         self.N2 = 100
@@ -65,6 +86,15 @@ class MainWindow(QMainWindow):
         self.t2 = QtCore.QTimer()
         self.t2.timeout.connect(self.plotData2)
         self.t2.start(20)
+        
+        self.p3 = self.pw.plot()
+        self.p3.setPen((0,170,0))
+        
+        self.p4 = self.pw.plot()
+        self.p4.setPen((0,85,255))
+        
+        self.p5 = self.pw.plot()
+        self.p5.setPen((255,85,255))
 
     def plotData(self):
         #print('plotData%d'% self.idx)
@@ -87,6 +117,83 @@ class MainWindow(QMainWindow):
             self.data2[-1] = tmp
         self.p2.setData(self.data2)
         self.idx2 += 1
+    
+    def funSelectColor1(self):
+        col = QColorDialog.getColor()
+        if col.isValid():
+            self.pushButtonColorSel1.setStyleSheet("QPushButton{background-color:%s}" % col.name())
+            self.p1.setPen(col.name())
+
+    def funSelectColor2(self):
+        col = QColorDialog.getColor()
+        if col.isValid():
+            self.pushButtonColorSel2.setStyleSheet("QPushButton{background-color:%s}" % col.name())
+            self.p2.setPen(col.name())
+            
+    def funSelectColor3(self):
+        col = QColorDialog.getColor()
+        if col.isValid():
+            self.pushButtonColorSel3.setStyleSheet("QPushButton{background-color:%s}" % col.name())
+            self.p3.setPen(col.name())
+  
+            
+    def funSelectColor4(self):
+        col = QColorDialog.getColor()
+        if col.isValid():
+            self.pushButtonColorSel4.setStyleSheet("QPushButton{background-color:%s}" % col.name())
+            self.p4.setPen(col.name())
+            
+    def funSelectColor5(self):
+        col = QColorDialog.getColor()
+        if col.isValid():
+            self.pushButtonColorSel5.setStyleSheet("QPushButton{background-color:%s}" % col.name())
+            self.p5.setPen(col.name())
+    
+    def detect_all_devices(self):
+        self.detect_all_serial_devices()
+        #self.start_thread_timer(self.find_all_devices, 1)
+        self.tDevDetect = QtCore.QTimer()
+        self.tDevDetect.timeout.connect(self.detect_all_serial_devices)
+        self.tDevDetect.start(1000)
+
+    def detect_all_serial_devices(self):
+        try:
+            if platform.system() == "Windows":
+                self.temp_serial = list()
+                for com in list(list_ports.comports()):
+                    try:
+                        strCom = com[0] #+ ": " + com[1][:-7]
+                    except:
+                        strCom = com.device #+ ": " + com.description
+                    self.temp_serial.append(strCom)
+                for item in self.temp_serial:
+                    if item not in self.serial_listbox:
+                        self.portName.addItem(item)
+                        #self.serial_frm.frm_left_listbox.insert("end", item)
+                for item in self.serial_listbox:
+                    if item not in self.temp_serial:
+                        size = self.portName.count()
+                        for index in range(size):
+                            if item == self.portName.itemText(index):
+                                self.portName.removeItem(index)
+
+                self.serial_listbox = self.temp_serial
+
+            elif platform.system() == "Linux":
+                self.temp_serial = list()
+                self.temp_serial = self.find_usb_tty()
+                for item in self.temp_serial:
+                    if item not in self.serial_listbox:
+                        self.portName.addItem(item)
+                for item in self.serial_listbox:
+                    if item not in self.temp_serial:
+                        size = self.portName.count()
+                        for index in range(size):
+                            if item == self.portName.itemText(index):
+                                self.portName.removeItem(index)
+                self.serial_listbox = self.temp_serial
+        except Exception as e:
+            logging.error(e)
     
     def slotCancel(self):
         self.t.stop()
